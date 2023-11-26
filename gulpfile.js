a9548ce4622cdccd5737b89f9d1c23191355ade3
Santiago <spretzsanti@gmail.com>
2023-11-25 23:55:07 -0300

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,57 @@
+const { src, dest, watch, series, parallel } = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const autoprefixer = require('autoprefixer');
+const postcss = require('gulp-postcss')
+const sourcemaps = require('gulp-sourcemaps')
+const cssnano = require('cssnano');
+const concat = require('gulp-concat');
+const terser = require('gulp-terser-js');
+const rename = require('gulp-rename');
+const notify = require('gulp-notify');
+const cache = require('gulp-cache');
+const clean = require('gulp-clean');
+
+
+
+const paths = {
+    scss: 'src/scss/**/*.scss',
+    js: 'src/js/**/*.js',
+    imagenes: 'src/img/**/*'
+}
+
+function css() {
+    return src(paths.scss)
+        .pipe(sourcemaps.init())
+        .pipe(sass())
+        .pipe(postcss([autoprefixer(), cssnano()]))
+        // .pipe(postcss([autoprefixer()]))
+        .pipe(sourcemaps.write('.'))
+        .pipe(dest('css/'));
+}
+
+function javascript() {
+    return src(paths.js)
+      .pipe(sourcemaps.init())
+      .pipe(concat('bundle.js'))
+      .pipe(terser())
+      .pipe(sourcemaps.write('.'))
+      .pipe(rename({ suffix: '.min' }))
+      .pipe(dest('./build/js'))
+}
+
+
+
+
+
+
+function watchArchivos() {
+    watch(paths.scss, css);
+    watch(paths.js, javascript);
+    
+    
+}
+
+exports.css = css;
+exports.watchArchivos = watchArchivos;
+exports.default = parallel(css, javascript,watchArchivos); 
+exports.dev = parallel (css,javascript,watchArchivos);
